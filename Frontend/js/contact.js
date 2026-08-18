b4ce934c8801d9946afac0e57ca88d60f2f8aab2
Umesh Pal <umeshpalshispn@gmail.com>
2026-08-18 15:00:35 +0530

    Update contact form

--- Frontend/js/contact.js
+++ Frontend/js/contact.js
@@ -1,74 +1,33 @@
-// Firebase SDK Import
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-
-// Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyDaCUJVGhMdgCuv2N3C36tnR2p7ZYvlAcI",
-  authDomain: "umesh-portfolio-bf3a2.firebaseapp.com",
-  projectId: "umesh-portfolio-bf3a2",
-  storageBucket: "umesh-portfolio-bf3a2.firebasestorage.app",
-  messagingSenderId: "920077072014",
-  appId: "1:920077072014:web:d3fa8554da4bfc0c6afbdf",
-};
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Form
-const form = 
-document.getElementById("contactForm");
-
-form.addEventListener("submit", async (e) => {
+const contactForm = document.querySelector("#contactForm");
+contactForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  const name = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const message = document.querySelector("#message").value.trim();
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const message = document.getElementById("message").value.trim();
-
-  const now = new Date();
-
-  try {
-    await addDoc(collection(db, "contacts"), {
-      name,
-      email,
-      message,
-      date:
-now.toLocaleDateString("en-IN"),
-      time: 
-now.toLocaleTimeString("en-IN"),
-      createdAt: serverTimestamp()
+  try{
+    const response = await fetch("http://localhost:5000/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        message
+      })
     });
-
-    // alert("✅ Message Sent Successfully!");
-    // form.reset();
-
-    Swal.fire({
-     icon:"success",
-     title:"Message Sent!",
-     text: "Thank you for contacting me. I'll get back to you soon.",
-     confirmButtonColor: "#c84cff",
-     width: window.innerWidth < 768 ? "300px" : "500px"
-    });
-    form.reset();
-
- }
- catch (error) {
+    const data = await response.json();
+    
+    if(response.ok && data.success) {
+      alert("Message sent successfully!");
+      contactForm.reset();
+    } else {
+      alert(data.message || "Please enter valid details.");
+    }
+  } catch (error) {
     console.error(error);
-    Swal.fire({
-        icon: "error",
-        title: "Oops!",
-        text: "Failed to send message. Please try again.",
-        confirmButtonColor: "#ff4d4d"
-    });
+    alert("Failed to send message");
   }
 });
